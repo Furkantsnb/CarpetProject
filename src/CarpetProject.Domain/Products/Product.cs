aa498ca9e3918511b6ebfbcc383bc1b2c738d242
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-08-15 13:01:31 +0300

    .

--- src/CarpetProject.Domain/Products/Product.cs
+++ src/CarpetProject.Domain/Products/Product.cs
@@ -13,24 +13,19 @@ namespace CarpetProject.Products
     {
         public int CategoryId { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
         public decimal Price { get; set; }
-        public int Stock { get;set; }
+        public string Description { get; set; }
+        public string? Ingredients { get; set; }//ürün içeriği
+        public string? Usage { get; set; }//ürün nasıl kullanılmalı
+        public string? AdditionalInfo { get; set; }//ek bilgi
         public int Discount { get; set; }//indirim yüzdesi
         public bool DiscountAvailable { get; set; }//indirim var mı?
         public bool IsApproved { get; set; }//Aktif mi değilmi Kontrolü
-        public bool IsHome { get; set; }//Ana sayfada gözüksün mü?
-        public string Ingredients { get; set; }//ürün içeriği
-        public string Usage { get; set; }//ürün nasıl kullanılmalı
-        public string AdditionalInfo { get; set; }//ek bilgi
         public DateTime ReleaseDate { get; set; } // Ürünün piyasaya çıkış tarihi
-        public double AverageRating { get; set; } // Ürünün ortalama puanı
-     
-
         public virtual ICollection<Category> Categories { get; set; }
         public virtual ICollection<ProductImage> ProductImages { get; set; }
         public virtual ICollection<Tag> Tags { get; set; } // Ürüne ait etiketler
-        public virtual ICollection<Comment> Comments { get; set; } // Ürüne yapılan yorumlar
+     
 
 
 
@@ -39,7 +34,7 @@ namespace CarpetProject.Products
             Categories = new HashSet<Category>();
             ProductImages = new HashSet<ProductImage>();
             Tags = new HashSet<Tag>();
-            Comments = new HashSet<Comment>(); // Yorumlar için koleksiyon başlatıldı
+        
 
         }
     }
